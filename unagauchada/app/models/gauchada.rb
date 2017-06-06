485a88cd7642b3851d5720f9350ba9cad001ee9d
pochy4545 <agustin.c.96@hotmail.com>
2017-06-05 22:08:55 -0300

    Se acomodo buscar

--- unagauchada/app/models/gauchada.rb
+++ unagauchada/app/models/gauchada.rb
@@ -3,4 +3,16 @@ class Gauchada < ApplicationRecord
 	belongs_to :user
 	has_attached_file :cover, styles: { medium:"800x800" ,thumb:"200x200" , mini: "50x50"}
 	validates_attachment :cover, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+
+
+	#filtrado
+	def self.search(search)
+		 if search 
+  			  where('title LIKE ?', "%#{search}%")
+
+  		else
+    		scoped
+  		end
+
+	end
 end
